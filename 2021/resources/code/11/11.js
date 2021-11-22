8890cf7c1fdeeec884e972ec89b386ab03653b5a
Nestor Napoles Lopez <napulen@gmail.com>
2021-11-22 15:02:41 -0500

    Adding auxiliary functions in separate file.

--- 2021/resources/code/11/11.js
+++ 2021/resources/code/11/11.js
@@ -1,53 +1,5 @@
 "use strict";
 
-function interval(frequency, semitones) {
-    // Assuming equal temperament
-    return frequency * Math.pow(2, semitones / 12);
-}
-
-function midiToFrequency(midinumber, concertA = 440) {
-    const A4 = 69
-    if (midinumber === A4) {
-        return concertA;
-    }
-    let semitones = midinumber - A4;
-    return interval(440, semitones);
-}
-
-function midiFromFrequency(frequency) {
-    let minDiff = Number.MAX_VALUE;
-    let midinumber = -1;
-    for (let midi = 0; midi < 128; midi++) {
-        let midiFreq = midiToFrequency(midi);
-        let freqDiff = Math.abs(midiFreq - frequency);
-        if (freqDiff < minDiff) {
-            minDiff = freqDiff;
-            midinumber = midi;
-        }
-    }
-    return midinumber
-}
-
-function noteFromFrequency(frequency, withOctave=false) {
-    let midinumber = midiFromFrequency(frequency);
-    let pitchclass = midinumber % 12;
-    let notenames = {
-        0: "C",
-        1: "C#/Db",
-        2: "D",
-        3: "D#/Eb",
-        4: "E",
-        5: "F",
-        6: "F#/Gb",
-        7: "G",
-        8: "G#/Ab",
-        9: "A",
-        10: "A#/Bb",
-        11: "B"
-    }
-    return notenames[pitchclass];
-}
-
 // Turn theremin on
 function thereminOn(oscillator) {
     oscillator.play();
@@ -104,6 +56,5 @@ function runAfterLoadingPage() {
         thereminOff(oscillator);
     });
 }
-
 
 window.onload = runAfterLoadingPage;
